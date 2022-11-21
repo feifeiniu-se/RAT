4b848ca468e946f35a0c83970fcdb5afeecccbbe
niufeifei0624 <victor_feifeiniu@outlook.com>
2022-11-21 10:04:06 +0100

    11.21

--- cache/run-rebuttal.py
+++ cache/run-rebuttal.py
@@ -43,7 +43,13 @@ def parser_log(file):
     result = cursor.fetchall()
     for tmp in result:
         mapping_files[tmp[0]] = tmp[1]
-    return mapping_files
+
+    renamed_fid = {v:set() for k,v in mapping_files.items()}
+    for k,v in mapping_files.items():
+        renamed_fid[v].add(k)
+    renamed_map = {k:v for k,v in renamed_fid.items() if len(v)>1}
+
+    return mapping_files, renamed_map
 
 def loadFileCommitHistory(commits):
     commits.sort(key=lambda x:x.commit_date)
@@ -60,8 +66,11 @@ def loadFileCommitHistory(commits):
 
 def isBugFixing(message):
     message = message.lower()
-    if "fix" in message or "bug" in message or file+"-" in message:
+    if "fix" in message or "bug" in message:
         return True
+    for id in bugids:
+        if id in message:
+            return True
     return False
 
 def versionHistoryCompute(issues, fileHistories):
@@ -83,35 +92,6 @@ def versionHistoryCompute(issues, fileHistories):
                     file_score[f] = score
         bug.cache_score = file_score
 
-def versionHistoryCompute2(issues, fileHistories):
-    test_set = set()
-    for bug in issues[:]:
-        end_date = bug.created_date
-        file_score = {} # filePath:score
-        for f,v in fileHistories.items():
-            v.sort(key=lambda x: x.commit_date)
-            start_date = v[0].commit_date
-            v = [commit for commit in v if isBugFixing(commit.message) and commit.commit_date<end_date]
-            # print(len(v))
-            # names = set(file_mapping[f] for x in v for f in x.files)
-            if f in set(x.filePath for x in issue.files):
-                print(f)
-                names = set(fp for x in v for fp in x.files if file_mapping[fp]==f)
-                print(names)
-                if len(names)>0:
-                    test_set.add(issue.issue_id)
-            if len(v)>0:
-                score = 0.0
-                rangeTime = float((end_date-start_date).total_seconds()/60)
-                for commit in v:
-                    normalized_t = float((commit.commit_date-start_date).total_seconds()/60) / rangeTime
-                    # print(normalized_t)
-                    score = score + float(1/(1+np.exp(-12*normalized_t+12)))
-                if score>0.0:
-                    file_score[f] = score
-        bug.bluir_score = file_score
-    return test_set
-
 
 def evaluate(bugs):
     print(len(bugs), end=";")
@@ -131,28 +111,12 @@ def evaluate(bugs):
 
     evaluation(ground_truth, predict_result)
 
-def evaluate2(bugs):
-    print(len(bugs), end=";")
-    # train_size = int(len(bugReport) * 0.8)
-    test_bugs = bugs[:]
 
-    for issue in test_bugs:
-        sorted_files2 = sorted(issue.cache_score.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-        issue.predict_bf = [x[0] for x in sorted_files2 if x[0] in issue.source_files]
-
-        sorted_files3 = sorted(issue.bluir_score.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-        issue.predict_bf_r = [x[0] for x in sorted_files3 if x[0] in issue.source_files]
-
-
-    ground_truth = [set(f.filePath for f in issue.files if f.filePath != "/dev/null" and f.filePath is not None) for issue in test_bugs if issue.issue_type=="Bug"]
-    predict_result = [issue.predict_bf for issue in test_bugs if issue.issue_type=="Bug"]
-
-    index = [i for i in range(len(ground_truth)) if len(ground_truth[i]) == 0]
-    ground_truth = [ground_truth[i] for i in range(len(ground_truth)) if i not in index]
-    predict_result = [predict_result[i] for i in range(len(predict_result)) if i not in index]
-    evaluation(ground_truth, predict_result)
-
-
+def IsRenamedIssue(issue, renamed_files):
+    for f in issue.files:
+        if f.filePath in renamed_files:
+            return True
+    return  False
 
 
 path = "C:/Users/Feifei/dataset/tracescore"
@@ -160,13 +124,14 @@ path2 = "C:/Users/Feifei/dataset/issues"
 files = ["derby", "drools", "hornetq", "izpack", "keycloak", "log4j2", "railo", "seam2", "teiid", "weld", "wildfly"]
 test_set = set()
 
-for file in files[2:]:
+for file in files[:]:
     # file = "wildfly.sqlite3"
     print(file, end=" ")
     filePath = path+"\\"+file + ".sqlite3"
     filePath2 = path2+"\\"+file+".sqlite3"
     issues = read_sqlite(filePath)
     issues = [x for x in issues if x.issue_type == "Bug"]
+    bugids = [x.issue_id.lower() for x in issues]
     # print(len(issues))
     commits = read_commits(filePath2)
     file_history = loadFileCommitHistory(commits) # {filePath: file commit history}
@@ -175,12 +140,20 @@ for file in files[2:]:
     # evaluate(issues)
 
     # convert all file_path to fid
-    file_mapping = parser_log(file)
+    file_mapping, renamed_map = parser_log(file)
+    renamed_files = []
+    for k, v in renamed_map.items():
+        renamed_files = renamed_files + list(v)
+
+    # issues = [x for x in issues if IsRenamedIssue(x, renamed_files)] #pick out issues that touch renamed files
+
+    versionHistoryCompute(issues, file_history)
+    evaluate(issues)
 
     file_history_ = {}
     for f,v in file_history.items():
         if f not in file_mapping:
-            file_mapping[f] = len(file_mapping)
+            file_mapping[f] = len(file_mapping)+10000
         fid = file_mapping[f]
         if fid in file_history_:
             file_history_[fid].extend(v)
@@ -193,10 +166,7 @@ for file in files[2:]:
             if issue.files[i].filePath != "/dev/null":
                 issue.files[i].filePath = file_mapping[issue.files[i].filePath]
 
-
-    test_set = versionHistoryCompute2(issues, file_history_)
-    print(test_set)
-    # print(";")
-    evaluate2(issues)
+    versionHistoryCompute(issues, file_history_)
+    evaluate(issues)
 
 
